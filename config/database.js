3f394063227c21517619d6f15e382e594de532c4
nguyencaotai1969 <nguyencaotai1969>
2021-09-26 22:05:02 +0700

    update

---
+++ config/database.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+	'url': 'mongodb://127.0.0.1:27017',
+	'options': {
+		//'user':   'kvcpro',
+		//'pass':   'Minhhoang2009@',
+		'dbName': 'rik88', // red
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex':       false,
+	},
+};
